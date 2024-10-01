df93b8f8178c577ba64e083478ed0ca79c19e9d8
lavesh-code <laveshg.work@gmail.com>
2024-10-01 15:41:54 +0530

    Time: 27 ms (7.11%) | Memory: 28.1 MB (15.20%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -12,16 +12,27 @@
 class Solution {
 private:
     bool isSameTree(TreeNode* s, TreeNode* t) {
-        if (s ==NULL && t==NULL) return true; // Both trees are empty
-        if (s==NULL || t==NULL) return false; // One tree is empty, and the other is not
-        if (s->val != t->val) return false; // Values do not match
+        // Both trees are empty
+        if (s ==NULL && t==NULL) return true;
+
+        // One tree is empty, and the other is not 
+        if (s==NULL || t==NULL) return false;
+
+        // Values do not match
+        if (s->val != t->val) return false; 
+
         // Recursively check left and right subtrees
         return isSameTree(s->left, t->left) && isSameTree(s->right, t->right);
     }
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if (root == NULL) return false; // If root is null, subRoot cannot be a subtree
-        if(isSameTree(root, subRoot)) return true; // Check if trees are identical
+        // If root is null, subRoot cannot be a subtree
+        if (root == NULL) return false; 
+
+        // INORDER LIKE TRAVERSAL WITH CHECKING AT EACH NODE - 
+        // Check if trees are identical
+        if(isSameTree(root, subRoot)) return true; 
+
         // Recursively check left and right subtrees
         return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
     }

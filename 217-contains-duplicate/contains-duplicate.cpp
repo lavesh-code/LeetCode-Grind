03106a2729ca99ae5c52579f1a67728cea93e259
lavesh-code <laveshg.work@gmail.com>
2024-10-01 18:36:02 +0530

    Time: 97 ms (44.89%) | Memory: 73.3 MB (70.80%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> map; // value : freq
+        for(int i=0; i <nums.size(); i++){
+            if(map.count(nums[i])){
+                return true; 
+            }
+            map[nums[i]] += 1;
+        }
+
+        return false; 
+        
+    }
+};

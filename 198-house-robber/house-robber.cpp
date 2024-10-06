cda6c4e61fb3198a6f2b621c447b1dbf5dc591d5
lavesh-code <laveshg.work@gmail.com>
2024-10-06 19:42:26 +0530

    Time: 4 ms (32.04%) | Memory: 9.5 MB (46.64%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -12,9 +12,19 @@ public:
     }
 
     int rob(vector<int>& nums) {
-        int start_index = 0;
+        // int start_index = 0;
         int n = nums.size();
         vector<int> dp(n+1, -1); 
-        return take_not_take(nums, dp, n-1);
+        // return take_not_take(nums, dp, n-1);
+        dp[0] = nums[0];
+        for(int i=1; i < nums.size(); i++){
+            int take = nums[i];
+            if(i>1) take += dp[i-2];
+            int not_take = dp[i-1];
+
+            dp[i] = max(take, not_take);
+        }
+
+        return dp[n-1]; 
     }
 };

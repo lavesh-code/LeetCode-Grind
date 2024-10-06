374f99cc403939bee59dd9f6b1a973b1c22227c6
lavesh-code <laveshg.work@gmail.com>
2024-10-06 16:59:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (32.47%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -4,7 +4,7 @@ public:
         if(n==0) return 0;
         if(n==1 || n==2) return 1;
 
-        vector<int> dp(n+2, -1);
+        vector<int> dp(n+1, -1);
         dp[0] = 0; dp[1] = 1; dp[2] = 1;
         
         for(int i = 3; i <=n ;i++){

b20851b3bedc55b3776ea9d717918ec805b26f8a
lavesh-code <laveshg.work@gmail.com>
2024-11-22 15:23:37 +0530

    Time: 51 ms (61.14%) | Memory: 29.4 MB (40.98%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,36 +1,45 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& arr) {
+    vector<vector<int>> threeSumUtil(vector<int>& arr, int target) {
         int n = arr.size();
         vector<vector<int>> ans;
-    sort(arr.begin(), arr.end()); // n logn
-    for (int i = 0; i < n; i++) {
-        //remove duplicates:
-        if (i != 0 && arr[i] == arr[i - 1]) continue;
-        //moving 2 pointers:
-        int j = i + 1;
-        int k = n - 1;
-        while (j < k) { // n^2 
-            int sum = arr[i] + arr[j] + arr[k];
-            if (sum < 0) {
-                j++;
-            }
-            else if (sum > 0) {
-                k--;
-            }
-            else {
-                vector<int> temp = {arr[i], arr[j], arr[k]};
-                ans.push_back(temp);
-                j++;
-                k--;
-                //skip the duplicates:
-                while (j < k && arr[j] == arr[j - 1]) j++;
-                while (j < k && arr[k] == arr[k + 1]) k--;
+        sort(arr.begin(), arr.end()); // n logn
+        
+        for (int i = 0; i < n; i++) {
+            // skip the duplicates:
+            if (i != 0 && arr[i] == arr[i - 1]) continue;
+            
+            // pointer i --> index i
+            // moving 2 pointers:
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) { // n^2 
+                int sum = arr[i] + arr[j] + arr[k];
+                if (sum < target) {
+                    j++;
+                }
+                else if (sum > target) {
+                    k--;
+                }
+                else {
+                    vector<int> temp = {arr[i], arr[j], arr[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    //skip the duplicates:
+                    while (j < k && arr[j] == arr[j - 1]) j++;
+                    while (j < k && arr[k] == arr[k + 1]) k--;
+                }
             }
         }
+        return ans;
     }
-    return ans;
 
+
+    vector<vector<int>> threeSum(vector<int>& arr){
+        int target = 0;
+        vector<vector<int>> ans = threeSumUtil(arr,target);
+        return ans; 
     }
 };
 

370d40e27ae138a34bfa4dfc0216c33574a3e03e
lavesh-code <laveshg.work@gmail.com>
2024-11-22 15:11:44 +0530

    Time: 8 ms (67.61%) | Memory: 10.8 MB (63.01%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -11,22 +11,34 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-
-        // min. one node where we START and reach BACK
-        // visited a node more than once
-
         ListNode* slow = head;
         ListNode* fast = head;
 
-        while(fast != NULL && fast->next !=NULL){
-            fast = fast->next->next;
-            slow = slow->next; 
+        while(fast != NULL && fast->next != NULL && slow != NULL){
+            slow = slow -> next;
+            fast = fast -> next -> next;
 
-            if(fast == slow){
+            if (slow == fast){
                 return true;
             }
         }
-        
-        return false; 
+        return false;
     }
+
+/* Imagine a race where one runner moves at twice the speed of another. The  faster runner covers the ground faster and closes the gap, resulting in a reduction in the distance between them. Similarly, the fast pointer catches up to the slow pointer in the looped linked list, closing in the gap between them until it reaches zero. */
+
+    //     ListNode* slow = head;
+    //     ListNode* fast = head;
+
+    //     while(fast != NULL && fast->next !=NULL){
+    //         fast = fast->next->next;
+    //         slow = slow->next; 
+
+    //         if(fast == slow){
+    //             return true;
+    //         }
+    //     }
+        
+    //     return false; 
+    // }
 };
